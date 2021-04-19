23058a19ab2545185335bed233793d6c18b49c7b
Achermiel <achermiel@gmail.com>
2021-04-19 19:54:17 -0300

    Muitas modificações
    matplotlib
    retirei weber e rizzi

--- webapp.py
+++ webapp.py
@@ -13,127 +13,90 @@ from functions import *
 import pandas as pd
 import os
 from PIL import Image, ImageOps
-
+import matplotlib.pyplot as plt
 
 st.title('Haziness')
-
 st.write("This web app calculates the Haziness measure.")
 st.write("""
          It can also calculate RMS, Histogram Spread,
-         Michelson, Weber and Rizzi metrics.
-         """)
+         Michelson, Weber and Rizzi metrics.""")
 
 calc_rms = st.checkbox('RMS')
-calc_Weber = st.checkbox('Weber')
+# calc_Weber = st.checkbox('Weber')
 calc_Michelson = st.checkbox('Michelson')
-calc_Rizzi = st.checkbox('Rizzi')
+# calc_Rizzi = st.checkbox('Rizzi')
 calc_HS = st.checkbox('Histogram Spread')
 
 rms_list = []
-Weber_list = []
+# Weber_list = []
 Michelson_list = []
-Rizzi_list = []
+# Rizzi_list = []
 HS_list = []
 
-N = st.number_input("Select number of iterations: ",min_value=1, step=1)
-s = st.number_input("Select the size of the patches: ",min_value=1, step=1)
+N = st.number_input("Select number of iterations: ",min_value=1, value=1000, step=1)
+s = st.number_input("Select the size of the patches: ",min_value=1, value=2, step=1)
 
 # import libraries
-import tkinter as tk
-from tkinter import filedialog
+# import tkinter as tk
+# from tkinter import filedialog
 
-# Set up tkinter
-root = tk.Tk()
-root.withdraw()
-
-# Make folder picker dialog appear on top of other windows
-root.wm_attributes('-topmost', 1)
-
-# Folder picker button
-# st.title('Folder Picker')
-# st.write('Please select a folder:')
-# clicked = st.button('Folder')
-# if clicked:
-#     dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
 
 # files = glob.glob(os.path.join(dirname,'/*'))
 multiple_files = st.file_uploader("Select files: ",accept_multiple_files=True)
 if multiple_files is None:
     st.text("No upload")
 else:
+    st.text("The files are:")
     files = [file.name for file in multiple_files]
     st.text("\n".join(files))
-# st.write(multiple_files[0].)
-    
-if len(files) == 0:
-    print("""
-The program couldn't identify the folder.
-Please check if the name of the folder is 'Images'.
-And it is not empty.
-          """)
 
 
-# st.image(multiple_files[0],use_column_width='auto')
 values = []
-for file in multiple_files:
-#     file2 = file.split("\\")[1]
-    print(f'Calculating for {file.name}')
-#     file = cv.imread(file, 0)
-    
-    img = Image.open(file)
-    img = ImageOps.grayscale(img)
-    img = np.asarray(img)
-    x, y = haziness_mean_std(img, N, s)
-    values.append([file.name, round(x, 4), round(y, 4)])
-    
-    
-    # Calculate other metrics
-    if calc_rms:
-        rms_list.append(rms(img))
-    if calc_Weber:
-        Weber_list.append(Weber(img))
-    if calc_Michelson:
-        Michelson_list.append(Michelson(img))
-    if calc_Rizzi:
-        Rizzi_list.append(Rizzi(img))
-    if calc_HS:
-        HS_list.append(HS(img))
-st.write(values)
-DF = pd.DataFrame(values,columns=["Name", "Value", "Std"])
+# @st.cache()
+def calculations():
+    for file in multiple_files:
+    #     file2 = file.split("\\")[1]
+        st.text(f'Calculating for {file.name}')
+    #     file = cv.imread(file, 0)
+
+        img = Image.open(file)
+        img = ImageOps.grayscale(img)
+        img = np.asarray(img)
+        x, y = haziness_mean_std(img, N, s)
+        values.append([file.name, round(x, 4), round(y, 4)])
 
 
+        # Calculate other metrics
+        if calc_rms:
+            rms_list.append(RMS(img))
+    #     if calc_Weber:
+    #         Weber_list.append(Weber(img))
+        if calc_Michelson:
+            Michelson_list.append(Michelson(img))
+    #     if calc_Rizzi:
+    #         Rizzi_list.append(Rizzi(img))
+        if calc_HS:
+            HS_list.append(HS(img))
 
-
-
+calculations()
+DF = pd.DataFrame(values,columns=["Name", "Haziness", "Std"])
+if calc_rms:
+    DF["RMS"] = rms_list
+if calc_Michelson:
+    DF["Michelson"] = Michelson_list
+if calc_HS:
+    DF["HS"] = HS_list
+DF.sort_values(by=['Name'],inplace=True,ignore_index=True)
 st.write(DF)
-# st.write(files)
-# if agree:
-#     st.write('Great!')
-
-# set PATH=%PATH%;C:\Users\T-Gamer\anaconda3;C:\Users\T-Gamer\anaconda3\Scripts\.
-# N = int(input("Select number of iterations: "))
-# s = int(input("Select the size of the patches: "))
 
 
-# files = glob.glob('Test_images\*')
-# if len(files) == 0:
-#     print("""
-# The program couldn't identify the folder.
-# Please check if the name of the folder is 'Images'.
-# And it is not empty.
-#           """)
-
-# files.sort()
-
-# values = []
-# for file in files:
-#     file2 = file.split("\\")[1]
-#     print(f'Calculating for {file2}')
-#     file = cv.imread(file, 0)
-#     x, y = haziness_mean_std(file, N, s)
-#     values.append([file2, round(x, 4), round(y, 4)])
-# # print(values)
-
-# print('Done')
-# DF = pd.DataFrame(values)
-# header = ["Name", "Value", "Std"]
+fig, ax = plt.subplots()
+ax.plot(range(len(values)),DF.Haziness,'ok--',label='Haziness')
+if calc_rms:
+    ax.plot(range(len(values)),rms_list,'or--',label='RMS')
+if calc_Michelson:
+    ax.plot(range(len(values)),Michelson_list,'ob--',label='Michelson')
+if calc_HS:
+    ax.plot(range(len(values)),HS_list,'og--',label='HS')
+ax.legend()
+st.pyplot(fig)
